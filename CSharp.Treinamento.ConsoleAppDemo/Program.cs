4e66058d887f2fb300b866b717c9a113a1059dda
Caio Brdaric <caiobrdaric@gmail.com>
2022-08-03 23:32:29 -0300

    Limpando area de estudos

--- CSharp.Treinamento.ConsoleAppDemo/Program.cs
+++ CSharp.Treinamento.ConsoleAppDemo/Program.cs
@@ -1,16 +1,2 @@
-﻿using CSharp.Treinamento.ConsoleAppDemo.Model;
-
-var trabalhador = new Trabalhador();
-
-Console.WriteLine("Digite o nome do trabalhador:");
-trabalhador.nome = Console.ReadLine();
-
-Console.WriteLine("Digite o salário do trabalhador");
-trabalhador.salario = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Digite o valor do bonus");
-double bonus = Convert.ToDouble(Console.ReadLine());
-trabalhador.AcrescentaBonus(bonus);
-
-Console.WriteLine($"O nome digitado foi:{trabalhador.nome}. E o salário digitado foi: {trabalhador.salario}");
+﻿
 
